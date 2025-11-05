18c8b1f010cdf43a737dfc92279e8a5d3d24eaf4
cindy <leti115@gmail.com>
2025-11-04 21:46:47 -0300

    PHP proyecto

--- Persistencia/EstudianteDB.php
+++ Persistencia/EstudianteDB.php
@@ -13,7 +13,7 @@ class EstudianteBD extends conecion{
     $this-> conectar();
     $sql= "INSERT INTO estudiante(cedula,correo,nombre,matricula,generacion) values (?,?,?,?,?)";    
     $stmt = $this->con->prepare($sql);
-    $stmt->bind_param(" isssi ",$cedula,$correo,$nombre,$matricula,$generacion);
+    $stmt->bind_param("isssi",$cedula,$correo,$nombre,$matricula,$generacion);
     
     if ($stmt->execute()){
         echo "nuevo registro creado con exito";
@@ -28,7 +28,7 @@ class EstudianteBD extends conecion{
 
     $this->conectar();
     $sql = "SELECT * FROM estudiante";
-    $resuly = $this->con->query($sql);
+    $result = $this->con->query($sql);
 
     $estudiante =[];
     if ($result->num_rows > 0 ){
